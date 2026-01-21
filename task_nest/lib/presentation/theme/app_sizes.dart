f188e189727862cdb179a9d40f4679eeeb851aa1
Alice <alice.akantinova19@gmail.com>
2026-01-21 12:41:14 +0300

    minor fix

--- task_nest/lib/presentation/theme/app_sizes.dart
+++ task_nest/lib/presentation/theme/app_sizes.dart
@@ -19,6 +19,7 @@ class AppSizes {
   static const double size4 = 4.0;
   static const double size12 = 12.0;
   static const double size18 = 18.0;
+  static const double size16 = 16.0;
   static const double size20 = 20.0;
   static const double size24 = 24.0;
   static const double size28 = 28.0;
